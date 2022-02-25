fe5e6e19b642bd0b4197eda2c5d3a47f6112f464
gaparovarlen <almamat2020@mail.ru>
2022-02-25 13:41:25 +0600

    init

--- project/settings.py
+++ project/settings.py
@@ -154,10 +154,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_USE_TLS = False
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = '1025'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-# EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend")
+# EMAIL_USE_TLS = False
+# # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = '1025'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend")
